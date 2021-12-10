6b0c24da2f3641769816d31866f570f375dfd865
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-12-10 21:37:25 +0400

    feat: add html element to toy

--- christmas-task/src/components/toy/toy.ts
+++ christmas-task/src/components/toy/toy.ts
@@ -1,6 +1,8 @@
 import {BaseComponent} from '../baseComponent/baseComponent';
+import {IToy}  from '../generalTypes/general';
+import './toy.css'
 
-class Toy extends BaseComponent {
+export class Toy extends BaseComponent {
     private numImage: number;
     private name: string;
     private count: number;
@@ -9,16 +11,62 @@ class Toy extends BaseComponent {
     private color: string;
     private size: string;
     private favorite: boolean;
-    constructor(numImage:number, name:string, count:number, year: number, shape:string, color:string, size:string, favorite:boolean){
+
+    private divName: HTMLDivElement;
+    private divImg: HTMLDivElement;
+    private divCount: HTMLDivElement;
+    private divYear: HTMLDivElement;
+    private divShape: HTMLDivElement;
+    private divColor: HTMLDivElement;
+    private divSize: HTMLDivElement;
+    private divFavorite: HTMLDivElement;
+    constructor(toyInfo:IToy){
         super('toy');
-        this.numImage = numImage;
-        this.name = name;
-        this.count = count;
-        this.year = year;
-        this.shape = shape;
-        this.color = color;
-        this.size = size;
-        this.favorite = favorite;
+        this.numImage = +toyInfo.num;
+        this.name = toyInfo.name;
+        this.count = +toyInfo.count;
+        this.year = +toyInfo.year;
+        this.shape = toyInfo.shape;
+        this.color = toyInfo.color;
+        this.size = toyInfo.size;
+        this.favorite = toyInfo.favorite;
+    }
+
+    init():void{
+        this.divName = document.createElement('div');
+        this.divName.className = 'font_name';
+        this.divName.innerHTML = this.name;
+        
+        this.divImg = document.createElement('div');
+        this.divImg.className = 'toy_picture';
+        this.divImg.style.backgroundImage = `url(./assets/toys/${this.numImage}.png)`;
+        
+        this.divCount = document.createElement('div');
+        this.divCount.className = 'font_info';
+        this.divCount.innerHTML = `Количество: ${this.count}`;
+
+        this.divYear = document.createElement('div');
+        this.divYear.className = 'font_info';
+        this.divYear.innerHTML = `Год покупки: ${this.year} год`;
+
+        this.divShape = document.createElement('div');
+        this.divShape.className = 'font_info';
+        this.divShape.innerHTML = `Форма игрушки: ${this.shape}`
+
+        this.divColor = document.createElement('div');
+        this.divColor.className = 'font_info';
+        this.divColor.innerHTML = `Цвет игрушки: ${this.color}`
+
+        this.divSize = document.createElement('div');
+        this.divSize.className = 'font_info';
+        this.divSize.innerHTML = `Размер игрушки: ${this.size}`;
+
+        this.divFavorite = document.createElement('div');
+        this.divFavorite.className = 'font_info';
+        this.divFavorite.innerHTML =`Любимая: ${(this.favorite) ? 'да': 'нет'}`
+        
+        this.node.append(this.divName, this.divImg, this.divCount, this.divYear,
+            this.divShape, this.divColor, this.divSize,this.divFavorite);
     }
 
     changeFavorite():void{
--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import {data} from './components/data';
+import {Toy} from './components/toy/toy';
+import {IToy} from './components/generalTypes/general'
+
+let toy = new Toy(<IToy>data[0]);
+toy.init();
+document.body.append(toy.node);
