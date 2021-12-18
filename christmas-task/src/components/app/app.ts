fa0312a366181ee0c915b7222548220cd0914411
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-12-18 23:45:44 +0400

    feat:add header

--- christmas-task/src/components/app/app.ts
+++ christmas-task/src/components/app/app.ts
@@ -1,4 +1,4 @@
-
+import './app.css';
 import {BaseComponent} from '../baseComponent/baseComponent';
 import { FilterToy } from '../filter/filterToy';
 import {data} from '../data';
@@ -12,6 +12,10 @@ export class App extends BaseComponent{
     private header: HTMLElement;
     private footer: HTMLElement;
     private filterToy: FilterToy;
+    private logo: HTMLDivElement;
+    private divToy: HTMLDivElement;
+    private divChristmasTree: HTMLDivElement;
+
     constructor(){
         super('app');
         data.forEach((item) => {
@@ -31,6 +35,29 @@ export class App extends BaseComponent{
         this.footer.className = 'footer';
 
         this.node.append(this.header, this.filterToy.node,this.footer);
+        this.createHeaderContent();
         parent.append(this.node);
+    }
+
+    createHeaderContent(){
+        const headerContainer = document.createElement('div');
+        headerContainer.className = 'header_container'
+
+        this.logo = document.createElement('div');
+        this.logo.className = 'logo'
+
+        this.divToy = document.createElement('div');
+        this.divToy.className = 'item_menu';
+        this.divToy.innerHTML = 'игрушки';
+        this.divChristmasTree = document.createElement('div')
+        this.divChristmasTree.className = 'item_menu';
+        this.divChristmasTree.innerHTML = 'елка';
+
+        const nav = document.createElement('nav');
+        nav.className = 'nav';
+        nav.append(this.logo, this.divToy, this.divChristmasTree);
+
+        headerContainer.append(nav);
+        this.header.append(headerContainer)
     }
 }
