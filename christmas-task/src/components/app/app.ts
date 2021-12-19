cf15cafa0ae050ae0690b89a1e29255badaf1073
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-12-19 12:51:33 +0400

    feat: add start page and footer

--- christmas-task/src/components/app/app.ts
+++ christmas-task/src/components/app/app.ts
@@ -11,6 +11,8 @@ export class App extends BaseComponent{
 
     private header: HTMLElement;
     private footer: HTMLElement;
+    private startPage: HTMLDivElement;
+    private startPageBtn: HTMLButtonElement;
     private filterToy: FilterToy;
     private logo: HTMLDivElement;
     private divToy: HTMLDivElement;
@@ -29,13 +31,30 @@ export class App extends BaseComponent{
         this.header = document.createElement('header');
         this.header.className = 'header';
 
+        this.startPage = document.createElement('div');
+        this.startPage.className = 'page start_page';
+        this.startPage.innerHTML = '<h1 class="start_page_title">Новогодняя игра<span>«Наряди ёлку»</span></h1>'
+        this.startPageBtn = document.createElement('button');
+        this.startPageBtn.className = 'start_btn';
+        this.startPageBtn.innerHTML = 'Начать';
+        this.startPageBtn.addEventListener('click', ()=>{
+            this.startGame();
+        })
+        this.startPage.append(this.startPageBtn);
         this.filterToy = new FilterToy(this.toy);
         this.filterToy.init();
         this.filterToy.node.classList.add('none');
         this.footer = document.createElement('footer');
         this.footer.className = 'footer';
+        this.footer.innerHTML = `<div class="footer_container">
+                    <a class="github" href="https://github.com/Mrak9087" target="blank">Mrak9087</a>
+                    <span class="rss_year">2021</span>
+                    <a class="rss" href="https://rs.school/js/" target="_blank" rel="noopener noreferrer">
+                        
+                    </a>
+                </div>`;
 
-        this.node.append(this.header, this.filterToy.node,this.footer);
+        this.node.append(this.header, this.startPage, this.filterToy.node,this.footer);
         this.createHeaderContent();
         parent.append(this.node);
     }
@@ -46,12 +65,16 @@ export class App extends BaseComponent{
 
         this.logo = document.createElement('div');
         this.logo.className = 'logo'
+        this.logo.addEventListener('click', ()=>{
+            this.filterToy.node.classList.add('none');
+            this.startPage.classList.remove('none');
+        })
 
         this.divToy = document.createElement('div');
         this.divToy.className = 'item_menu';
         this.divToy.innerHTML = 'игрушки';
         this.divToy.addEventListener('click', ()=>{
-            this.filterToy.node.classList.remove('none');
+            this.startGame()
         })
         this.divChristmasTree = document.createElement('div')
         this.divChristmasTree.className = 'item_menu';
@@ -63,5 +86,10 @@ export class App extends BaseComponent{
 
         headerContainer.append(nav);
         this.header.append(headerContainer)
+    }
+
+    startGame = () => {
+        this.startPage.classList.add('none');
+        this.filterToy.node.classList.remove('none');
     }
 }
