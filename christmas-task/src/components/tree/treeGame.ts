871acdbc5bcb31040d90733e8f77faf7e6293bf8
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-12-22 17:47:18 +0400

    feat: add view game

--- christmas-task/src/components/tree/treeGame.ts
+++ christmas-task/src/components/tree/treeGame.ts
@@ -10,9 +10,11 @@ export class TreeGame extends BaseComponent{
     private thumbBgs:ThumbBg[] = [];
     private containerDiv:HTMLDivElement;
     private settingDiv:HTMLDivElement;
+    private toyTreeDiv:HTMLDivElement;
     private viewGameDiv:HTMLDivElement;
     private mapElement:HTMLMapElement;
     private areaElement:HTMLAreaElement;
+    private imageTree:HTMLImageElement;
     constructor(){
         super('page tree_game');
     }
@@ -23,11 +25,7 @@ export class TreeGame extends BaseComponent{
         
         this.createSetting();
         this.createViewGame();
-        this.mapElement = document.createElement('map');
-        this.areaElement = document.createElement('area');
-        this.areaElement.coords = '365,699,189,706,113,683,31,608,2,555,2,539,18,437,73,351,106,224,161,134,243,-1,306,75,353,144,399,221,424,359,452,459,496,550,444,664';
-        this.areaElement.shape = 'poly';
-        this.mapElement.append(this.areaElement);
+        
 
         this.containerDiv.append(this.settingDiv,this.viewGameDiv)
         this.node.append(this.containerDiv);
@@ -65,5 +63,26 @@ export class TreeGame extends BaseComponent{
 
     createViewGame(){
         this.viewGameDiv = document.createElement('div');
+        this.viewGameDiv.className = 'view_game';
+        this.viewGameDiv.style.cssText = `background-image: url(./assets/bg/${this.thumbBgs[0].getBgImage()}.jpg)`;
+        this.mapElement = document.createElement('map');
+        this.mapElement.className = 'tree_card_map';
+        this.mapElement.name = 'tree_map';
+        this.areaElement = document.createElement('area');
+        this.areaElement.coords = '365,699,189,706,113,683,31,608,2,555,2,539,18,437,73,351,106,224,161,134,243,-1,306,75,353,144,399,221,424,359,452,459,496,550,444,664';
+        this.areaElement.shape = 'poly';
+        this.mapElement.append(this.areaElement);
+        this.imageTree = document.createElement('img');
+        this.imageTree.className = 'tree_img';
+        this.imageTree.alt = 'tree';
+        this.imageTree.useMap = '#tree_map';
+        this.imageTree.src = `./assets/tree/${this.thumbTrees[0].getBgImage()}.png`
+
+        this.viewGameDiv.append(this.mapElement,this.imageTree)
+    }
+
+    createToyTree(){
+        this.toyTreeDiv = document.createElement('div');
+        this.toyTreeDiv.className = 'toy_tree';
     }
 }
