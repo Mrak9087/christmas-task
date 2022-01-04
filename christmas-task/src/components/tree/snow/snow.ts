183b6409dd1e19dcd70bd9f8a763c718bec160bf
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-01-04 23:05:37 +0400

    feat: add helpers to snow

--- christmas-task/src/components/tree/snow/snow.ts
+++ christmas-task/src/components/tree/snow/snow.ts
@@ -1,20 +1,19 @@
 import './snow.css';
 import { BaseComponent } from '../../baseComponent/baseComponent';
+import { createHTMLElement } from '../../helpers/helpers';
 
 export class Snow extends BaseComponent {
-    public startSnowBtn: HTMLButtonElement;
+    public startSnowBtn: HTMLElement;
     private isStart: boolean;
     constructor() {
         super('snowflakes snow_hide');
         this.isStart = false;
-        this.startSnowBtn = document.createElement('button');
-        this.startSnowBtn.className = 'snow_btn';
+        this.startSnowBtn = createHTMLElement('button','snow_btn');
         this.startSnowBtn.addEventListener('click', () => {
             this.handleStartStopSnow();
         });
         this.createSnowFlake();
         this.isStart = localStorage.getItem('mrk90_snow') === 'true' ? true : false;
-        console.log('snow', this.isStart);
         if (this.isStart) {
             this.node.classList.remove('snow_hide');
         }
