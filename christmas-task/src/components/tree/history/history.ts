3007e6a345ec382c853ae0d7ac6ce5daa4654544
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-01-04 23:02:52 +0400

    feat: add helpers to history

--- christmas-task/src/components/tree/history/history.ts
+++ christmas-task/src/components/tree/history/history.ts
@@ -2,23 +2,20 @@ import './history.css';
 import { BaseComponent } from '../../baseComponent/baseComponent';
 import { treeImages } from '../../generalTypes/constants';
 import { ThumbTree } from '../thumbTree/thumbTree';
+import { createHTMLElement } from '../../helpers/helpers';
 
 export class History extends BaseComponent {
     private thumbTrees: ThumbTree[] = [];
-    btnSave: HTMLButtonElement;
+    btnSave: HTMLElement;
     constructor() {
         super('history_container');
     }
 
     init() {
-        this.btnSave = document.createElement('button');
-        this.btnSave.className = 'btn btn_history';
-        this.btnSave.innerHTML = 'Сохранить';
-        const head = document.createElement('h2');
-        head.innerHTML = 'Вы нарядили';
+        this.btnSave = createHTMLElement('button', 'btn btn_history', 'Сохранить');
+        const head = createHTMLElement('h2','','Вы нарядили');
 
-        const history = document.createElement('div');
-        history.className = 'history';
+        const history = createHTMLElement('div', 'history');
         treeImages.forEach((item) => {
             const thumb = new ThumbTree(item);
             history.append(thumb.node);
